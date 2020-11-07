9f48341e49110b11333100fdea77c72c60c6d13a
LiKunlong <lkl0801@sina.cn>
2020-11-07 13:19:25 +0800

    加油

--- 20201105/test.c
+++ 20201105/test.c
@@ -4,3 +4,22 @@ int main()
    printf("第一次提交代码!!\n");
    return 0;
 }
+
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int coding = 0;
+	printf("你会去敲代码吗？（选择1 or 0）\n");
+	scanf("%d", &coding);
+	if (coding == 1)
+	{
+		printf("坚持，你会有好offer\n");
+	}
+	else
+	{
+		printf("放弃，回家卖红薯\n");
+	}
+	return 0;
+}
