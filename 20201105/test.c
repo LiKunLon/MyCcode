897db1f5511e940286cf98a58548171770655c8b
LiKunlong <lkl0801@sina.cn>
2020-11-09 17:05:53 +0800

    提交代码

--- 20201105/test.c
+++ 20201105/test.c
@@ -48,3 +48,37 @@ int main()
 	
 	return 0;
 }
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <windows.h>
+int main()
+{
+	int a;
+	int b;
+	int c;
+	int t;
+	printf("请输入三个数字\n");
+	scanf("%d %d %d", &a, &b, &c);
+	if (a < b)//把a,b中较大的数给a
+	{
+		t = a;
+		a = b;
+		b = t;
+	}
+	if (a < c)//把a,c中较大的数给a
+	{
+		t = a;
+		a = c;
+		c = t;
+	}
+	if (b < c)
+	{
+		t = b;
+		b = c;
+		c = t;
+	}
+	printf("%d %d %d\n", a, b, c);
+	system("pause");
+	return 0;
+}
