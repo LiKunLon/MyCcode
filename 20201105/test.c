646cdb0530bd20c5f3993b161ae2f85e7734c10b
LiKunlong <lkl0801@sina.cn>
2020-11-10 19:28:48 +0800

    提交代码

--- 20201105/test.c
+++ 20201105/test.c
@@ -82,3 +82,52 @@ int main()
 	system("pause");
 	return 0;
 }
+
+
+打印3的倍数
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int i;
+	for (i = 1; i<=100; i++)
+	{
+		if (i % 3 == 0)
+			printf("%d\n", i);
+	}
+	return 0;
+}
+
+打印1000-2000间的闰年
+/*#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int i;
+	for (i = 1000; i <= 2000; i++)
+	{
+		if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+			printf("%d\n", i);
+	}
+	return 0;
+}*/
+
+
+打印100-200之间的素数
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int i,j;
+	for (i = 100; i <= 200; i++)
+	{
+		for (j=2; j <=i; j++)//从2开始循环
+		{
+			if (i%j == 0)//符合的话,说明不是素数
+				break;//循环终止
+		}
+		if (j >= i)//循环到i本身都没有能除尽的,是素数,输出
+			printf("%d\n", i);
+	}
+	return 0;
+}
