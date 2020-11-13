21939551c99c31093c1250624022ff5592397442
LiKunlong <lkl0801@sina.cn>
2020-11-13 09:10:24 +0800

    提交代码

--- codeStart/test.c
+++ codeStart/test.c
@@ -120,3 +120,132 @@ int main()
 	printf("最大数为:%d", max);
 	return 0;
 }
+
+
+输出99乘法表
+#include<stdio.h>
+int main()
+{
+	int i, j;
+	for (i = 1; i < 10; i++)//1=10循环
+	{
+		for (j = 1; j < 10; j++)//1-10循环
+			printf("%d*%d=%d ", i, j, i*j);//输出格式,注意有空格(美化)
+		printf("\n");//换行(美化)
+	}
+	
+	return 0;
+}
+
+
+写一个函数求两个数较大值
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int Max(int a,int b)
+{
+	if (a > b)//如果a>b,返回较大值a
+		return a;
+	else
+	{
+		return b;
+	}
+}
+int main()
+{
+	int a = 0;
+	int	b = 0;
+	printf("请输入两个待比较数:\n");
+	scanf("%d%d", &a,&b);
+	int ret = Max(a, b);//方法调用完毕,ret用来接收返回值
+	printf("较大数为:%d\n", ret);//直接调用Max函数
+	return 0;
+}
+
+分数求和
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int n;
+	float i = 1.0;//注意这里定义的是分子的1,必须是float型
+	int j = 1;
+	float sum = 0;
+	for (n = 1; n <= 100; n++)//循环100个数出来
+	{
+		sum = sum + (i/ n)*j;//数学表达式
+		j = -j;//用于正负号转换
+		
+	}
+	printf("%f\n", sum);
+	return 0;
+}
+
+写出一个程序，接受一个正浮点数值，输出该数值的近似整数值。如果小数点后数值大于等于5, 向上取整；小于5，则向下取整。
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	float i;
+	printf("请输入一个浮点数:\n");
+	scanf("%f", &i);
+	int num = (int)i;//强制变成整数
+	float a = i - num;
+	int b = 10 * a;
+	if (b >= 5)
+	{
+		printf("该浮点数向上取整:%d\n", num + 1);
+	}
+	else
+	{
+		printf("该浮点数向下取整:%d\n", num);
+	}
+	return 0;
+}
+不引入新临时变量,交换两个数
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("%d %d", a, b);
+}
+
+给定两个整形变量,进行交换
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a;
+	int b;
+	int t = 0;
+	scanf("%d %d", &a, &b);
+	t = a;
+	a = b;
+	b = t;
+	printf("%d %d\n", a, b);
+	return 0;
+}
+
+求数组元素个数并遍历
+#pragma warning (disable 4996)
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[] = { 1, 2, 3, 4, 5};
+	int num = sizeof(arr) / sizeof(arr[0]);//求数组元素个数
+	printf("%d\n", num);
+	//遍历(依次访问一遍且只访问一次)
+	int sum = 0;
+	for (; i < num; i++)
+	{
+		printf("arr[%d]=%d\n", i, arr[i]);
+		sum = sum + arr[i];	//数组所有元素之和
+	}
+	printf("%d\n", sum);
+	return 0;
+}
