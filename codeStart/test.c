733abd16d23a03dd24f67c8045ed145d93f512af
LiKunlong <lkl0801@sina.cn>
2020-11-15 03:15:56 +0800

    提交代码

--- codeStart/test.c
+++ codeStart/test.c
@@ -1269,3 +1269,58 @@ int main()
 	printf("%d\n", a);
 	return 0;
 }
+
+写一个函数返回参数二进制中 1 的个数。
+①
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int num = 10;
+	int count = 0;//计数
+	while (num)
+	{
+		if (num % 2 == 1)
+			count++;
+		num = num / 2;
+	}
+	printf("二进制中1的个数 = %d\n", count);
+	return 0;
+}
+//②
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int num = -1;
+	int i = 0;
+	int count = 0;//计数
+	for (i = 0; i<32; i++)
+	{
+		if (((num >> i) & 1) == 1)
+			count++;
+	}
+	printf("二进制中1的个数 = %d\n", count);
+	return 0;
+}
+
+
+③
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int num;
+	scanf("%d", &num);
+	int i = 0;
+	int count = 0;//计数
+	while (num)
+	{
+		count++;
+		num = num&(num - 1);
+	}
+	printf("二进制中1的个数 = %d\n", count);
+	return 0;
+}
+
+
