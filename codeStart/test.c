b845a582a0e831f6939144ef62fd507828fdf291
LiKunlong <lkl0801@sina.cn>
2020-11-16 10:24:01 +0800

    提交代码

--- codeStart/test.c
+++ codeStart/test.c
@@ -1340,4 +1340,22 @@ int main()
 }
 
 
+写一个函数打印arr数组的内容，不使用数组下标，使用指针。
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void pri(int *arr,int len)
+{
+	int *p = arr;
+	for (int i = 0; i < len; i++)
+	{
+		printf("%d ", *(p+i));
+	}
+}
 
+int main()
+{
+	int arr[5] = { 1, 2, 3, 4, 5 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	pri(arr, len);
+	return 0;
+}
