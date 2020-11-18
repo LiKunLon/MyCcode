30e90303ff5e527c7d910a06911cdabf0701fc39
LiKunlong <lkl0801@sina.cn>
2020-11-18 15:02:15 +0800

    提交代码

--- codeStart/test.c
+++ codeStart/test.c
@@ -1380,3 +1380,29 @@ int main()
 	printf("%d\n", sum);
 	return 0;
 
+
+函数实现字符串逆序,要求使用指针
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<string.h>
+void reverse(char* str)
+{
+	char* left = str;//数组名代表首元素地址
+	char* right = str + strlen(str) - 1;//str加数组元素个数等于最后一个元素的地址
+	while (left < right)
+	{
+		char tmp = *left;
+		*left = *right;
+		*right = tmp;
+		++left;
+		--right;
+	}
+}
+
+int main()
+{
+	char str1[] = "woaiLiLan";
+	reverse(str1);
+	printf("%s\n", str1);
+	return 0;
+}
