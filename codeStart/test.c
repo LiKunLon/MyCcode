5c9abd10a389cfcde8342b26d0fc7bbc7d327cb1
LiKunlong <lkl0801@sina.cn>
2020-11-20 09:20:38 +0800

    提交代码

--- codeStart/test.c
+++ codeStart/test.c
@@ -1457,3 +1457,22 @@ int main()
 	printf("%d", &arr+1);
 	return 0;
 }
+
+#include<stdio.h>
+int main()
+{
+	char *c[] = { "ENTER", "NEW", "POINT", "FIRST" };
+	char**cp[] = { c + 3, c + 2, c + 1, c };
+	char***cpp = cp;
+	printf("%s\n", **++cpp);//POINT 
+	printf("%s\n", *--*++cpp + 3);//ER
+	printf("%s\n", *cpp[-2] + 3);//ST
+	printf("%s\n", cpp[-1][-1] + 1);//EW
+
+	/*char* a[] = { "work", "at", "alibaba" };
+	char**pa = a;
+	pa++;
+	printf("%s\n", *pa);*/
+	return 0;
+}
+
