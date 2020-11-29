a3df143a65e97e33f719bce13cd66e2530165ef8
LiKunlong <lkl0801@sina.cn>
2020-11-29 21:26:19 +0800

    修改一下

--- codeStart/通讯录.c
+++ codeStart/通讯录.c
@@ -20,12 +20,14 @@ typedef struct Contact
 }Contact;
 void menu()
 {
+	printf("*****************************\n");
 	printf("**********这个b的通讯录******\n");
 	printf("*************1.add***********\n");
 	printf("*************2.search********\n");
 	printf("*************3.delete********\n");
 	printf("*************4.show**********\n");
 	printf("*************5.clear*********\n");
+	printf("*************6.change*********\n");
 	printf("*************0.quit**********\n");
 	printf("*****************************\n");
 }
@@ -37,6 +39,7 @@ void InitContact(Contact *p)
 }
 void AddContact(Contact *p)
 {
+	assert(p != NULL);
 	if (p->usedSize == MAX_SIZE)
 	{
 		printf("这个b的通讯录满了!\n");
@@ -58,6 +61,7 @@ void AddContact(Contact *p)
 //找不到返回-1,找到返回下标
 int SearchContact(Contact *p)
 {
+	assert(p != NULL);
 	int i = 0;
 	char name[20];
 	if (p->usedSize == 0)
@@ -72,12 +76,14 @@ int SearchContact(Contact *p)
 		if ((strcmp(p->per[i].name, name)) == 0)
 		{
 			return i;
+			printf("找到了!\n");
 		}
 	}
 	return -1;
 }
 void DeleteContact(Contact *p)
 {
+	assert(p != NULL);
 	int index = SearchContact(p);
 	if (index == -1)
 	{
@@ -94,6 +100,7 @@ void DeleteContact(Contact *p)
 }
 void ShowContact(Contact *p)
 {
+	assert(p != NULL);
 	int i = 0;
 	printf("%-10s %-10s %-10s %-10s %-10s\n", "姓名" "性别" "年龄" "电话" "住址");
 	for (i = 0; i < p->usedSize; i++)
@@ -101,9 +108,84 @@ void ShowContact(Contact *p)
 		printf("%-10s %-10s %-10s %-10s %-10s\n", p->per[i].name, p->per[i].sex, p->per[i].age, p->per[i].tel, p->per[i].address);
 	}
 }
+void ChangeContact(Contact *p)
+{
+	assert(p != NULL);
+	if (p->usedSize == 0)
+	{
+		printf("这个通讯录还是个空的,先添加一个再修改吧!\n");
+		return;
+	}
+	printf("开始修改!\n");
+	printf("请输入所要修改联系人的名字:\n");
+	char mingzi[10];
+	scanf("%s", mingzi);
+	int i = 0;
+	for (; i < p->usedSize; i++)
+	{
+		if ((strcmp(p->per[i].name, mingzi) == 0))
+			break;
+	}
+	printf("修改一项请按1,修改全部请按5!\n");
+	int input = 0;
+	int in = 0;
+	scanf("%d", &in);
+	switch (in)
+	{
+	case 1:
+		printf("请输入你要修改的信息:1.姓名 2.性别 3.年龄 4.电话 5.住址\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			printf("请输入修改后的姓名:\n");
+			scanf("%s", p->per[i].name);
+			break;
+		case 2:
+			printf("请输入修改后的性别:\n");
+			scanf("%s", p->per[i].sex);
+			break;
+		case 3:
+			printf("请输入修改后的年龄:\n");
+			scanf("%s", &(p->per[i].age));
+			break;
+		case 4:
+			printf("请输入修改后的电话:\n");
+			scanf("%s", p->per[i].tel);
+			break;
+		case 5:
+			printf("请输入修改后的住址:\n");
+			scanf("%s", p->per[i].address);
+			break;
+		default:
+			printf("您的输入有误!\n");
+			break;
+		}
+		break;
+	case 5:
+		printf("请输入修改后的名字:\n");
+		scanf("%s", p->per[i].name);
+		printf("请输入修改后的性别:\n");
+		scanf("%s", p->per[i].sex);
+		printf("请输入修改后的年龄:\n");
+		scanf("%s",&( p->per[i].age));
+		printf("请输入修改后的电话:\n");
+		scanf("%s", p->per[i].tel);
+		printf("请输入修改后的住址:\n");
+		scanf("%s", p->per[i].address);
+		break;
+		printf("修改成功!\n");
+	default :
+		printf("你的输入有误!\n");
+		break;
+	}
+	
+}
 void ClearContact(Contact *p)
 {
+	assert(p != NULL);
 	p->usedSize = 0;
+	printf("清理成功!\n");
 }
 int main()
 {
@@ -132,6 +214,9 @@ int main()
 			break;
 		case 5:
 			ClearContact(&con);
+			break;
+		case 6:
+			ChangeContact(&con);
 			break;
 		case 0:
 			quit = 1;
